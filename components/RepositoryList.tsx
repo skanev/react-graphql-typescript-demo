b22a1ba7564dd178c9d8193c32ef225b48a1b6ec
Stefan Kanev <stefan.kanev@gmail.com>
2021-12-07 14:09:01 +0200

    Part 2

--- components/RepositoryList.tsx
+++ components/RepositoryList.tsx
@@ -1,6 +1,13 @@
-import {gql, useQuery} from '@apollo/client'
+import {gql, useMutation, useQuery} from '@apollo/client'
 import Link from 'next/link'
-import {GetOrganizationQuery, GetOrganizationQueryVariables} from '../graph/types'
+import {
+  GetOrganizationQuery,
+  GetOrganizationQueryVariables,
+  StarRepositoryMutation,
+  StarRepositoryMutationVariables,
+  UnstarRepositoryMutation,
+  UnstarRepositoryMutationVariables
+} from '../graph/types'
 import {Spinner} from './Spinner'
 
 const GET_ORGANIZATION_QUERY = gql`
@@ -18,11 +25,44 @@ const GET_ORGANIZATION_QUERY = gql`
   }
 `
 
+const STAR_REPOSITORY_MUTATION = gql`
+  mutation StarRepositoryMutation($id: ID!) {
+    addStar(input: {starrableId: $id}) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
+
+const UNSTAR_REPOSITORY_MUTATION = gql`
+  mutation UnstarRepositoryMutation($id: ID!) {
+    removeStar(input: {starrableId: $id}) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
+
 export function RepositoryList({login}: {login: string}) {
   const {data, loading, error} = useQuery<GetOrganizationQuery, GetOrganizationQueryVariables>(GET_ORGANIZATION_QUERY, {
     ssr: false,
     variables: {login}
   })
+
+  const [star] = useMutation<StarRepositoryMutation, StarRepositoryMutationVariables>(STAR_REPOSITORY_MUTATION)
+  const [unstar] = useMutation<UnstarRepositoryMutation, UnstarRepositoryMutationVariables>(UNSTAR_REPOSITORY_MUTATION)
+
+  function toggle({id, starred}: {id: string; starred: boolean}) {
+    if (starred) {
+      return star({variables: {id}})
+    } else {
+      return unstar({variables: {id}})
+    }
+  }
 
   if (loading)
     return (
@@ -40,7 +80,7 @@ export function RepositoryList({login}: {login: string}) {
             <Link href={`/org/${login}/repo/${repository.name}`}>
               <a>{repository.name}</a>
             </Link>
-            <button>
+            <button onClick={() => toggle({id: repository.id, starred: !repository.viewerHasStarred})}>
               <span>{repository.stargazerCount}</span> {repository.viewerHasStarred ? '★' : '☆'}
             </button>
           </li>
--- graph/types.ts
+++ graph/types.ts
@@ -33,6 +33,60 @@ export interface GetOrganizationQueryVariables {
   login: string;
 }
 
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: StarRepositoryMutation
+// ====================================================
+
+export interface StarRepositoryMutation_addStar_starrable {
+  __typename: "Repository" | "Topic" | "Gist";
+  id: string;
+  viewerHasStarred: boolean;  // Returns a boolean indicating whether the viewing user has starred this starrable.
+}
+
+export interface StarRepositoryMutation_addStar {
+  __typename: "AddStarPayload";
+  starrable: StarRepositoryMutation_addStar_starrable | null;  // The starrable.
+}
+
+export interface StarRepositoryMutation {
+  addStar: StarRepositoryMutation_addStar;  // Adds a star to a Starrable.
+}
+
+export interface StarRepositoryMutationVariables {
+  id: string;
+}
+
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: UnstarRepositoryMutation
+// ====================================================
+
+export interface UnstarRepositoryMutation_removeStar_starrable {
+  __typename: "Repository" | "Topic" | "Gist";
+  id: string;
+  viewerHasStarred: boolean;  // Returns a boolean indicating whether the viewing user has starred this starrable.
+}
+
+export interface UnstarRepositoryMutation_removeStar {
+  __typename: "RemoveStarPayload";
+  starrable: UnstarRepositoryMutation_removeStar_starrable | null;  // The starrable.
+}
+
+export interface UnstarRepositoryMutation {
+  removeStar: UnstarRepositoryMutation_removeStar;  // Removes a star from a Starrable.
+}
+
+export interface UnstarRepositoryMutationVariables {
+  id: string;
+}
+
 /* tslint:disable */
 // This file was automatically generated and should not be edited.
 
