170c88948c76fd0a005a5d22fb929bc58762846b
Stefan Kanev <stefan.kanev@gmail.com>
2021-12-07 13:46:16 +0200

    Fix weird github types

--- apollo-codegen.sh
+++ apollo-codegen.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env zsh
 apollo-codegen generate '{pages,components,hooks}/**/*.{ts,tsx,graphql}' --schema schema.json --add-typename --target typescript --output graph/types.ts
+cat graph/types.ts | perl -npe '/nodes|addStar:|removeStar:|repository:|organization:/ && s/ \| null//g' > graph/types2.ts
+mv graph/types2.ts graph/types.ts
